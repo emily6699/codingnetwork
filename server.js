14ee6b1ca75972bb07a126b33ddb6ba361eedaee
emily <emilylu369@gmail.com>
2019-06-13 21:35:10 -0400

    create Users model schema

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.get("/", (req, res) => res.send("API Running"));
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-
 app.use("/api/profile", require("./routes/api/profile"));
-
 app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
