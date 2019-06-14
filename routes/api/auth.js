1f25e8a3eef7fdccba50041552638d1cc998d47c
emily <emilylu369@gmail.com>
2019-06-14 17:46:49 -0400

    set user auth route by using auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/Users");
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-router.get("/", (req, res) => res.send("auth  route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    // ('-password) means do not select password
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
