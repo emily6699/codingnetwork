0ddf600b476379f83a31e506bdf47f5dff0d11b1
emily <emilylu369@gmail.com>
2019-06-14 20:44:43 -0400

    routes for getting all the profiles and get sinlge profle by user id

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,6 +19,6 @@ module.exports = function(req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).jdon({ msg: "Token is not valid" });
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
