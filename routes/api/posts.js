a31a21fbda2af01e3f03159373c7e9b8246fb187
emily <emilylu369@gmail.com>
2019-06-15 23:16:35 -0400

    Get route to get single post by post id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -49,11 +49,33 @@ router.post(
 
 router.get("/", auth, async (req, res) => {
   try {
-    // sort by the most recent -1
+    // sort by the most recent by using -1
     const posts = await Post.find().sort({ date: -1 });
     res.json(posts);
-  } catch (error) {
+  } catch (err) {
     console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    GET api/posts/:id
+// @desc     Get  posts by ID
+// @access   Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    // sort by the most recent -1
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
     res.status(500).send("Server Error");
   }
 });
