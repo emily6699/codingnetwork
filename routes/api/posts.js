a843b0f6815f9bfd87da37f1242967de04c0782a
emily <emilylu369@gmail.com>
2019-06-15 23:07:08 -0400

    Get route to get all the posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -43,4 +43,19 @@ router.post(
   }
 );
 
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    // sort by the most recent -1
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
