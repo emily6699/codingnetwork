9adb4b9ab6e3ac7e34fb4c843834ffef2cdd3db0
emily <emilylu369@gmail.com>
2019-07-01 23:23:27 -0400

    added profile form if create profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-form/CreateProfile";
 import PrivateRoute from "./components/routing/privateRoute";
 //Redux
 import { Provider } from "react-redux";
@@ -38,6 +39,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/Create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
