fcb39cc655276ca595641fdbaca055c031922d08
emily <emilylu369@gmail.com>
2019-07-03 13:38:22 -0400

    created profile component after click on view profile

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/routing/privateRoute";
 import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -43,6 +44,8 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
+
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
