363193076f974dfb870ee3e88a294cfe46a1c086
emily <emilylu369@gmail.com>
2019-07-03 14:35:21 -0400

    profileEducation

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
@@ -50,6 +51,21 @@ const Profile = ({
                 </Fragment>
               ) : (
                 <h4>No experience </h4>
+              )}
+            </div>
+            <div className='profile-edu bg white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education </h4>
               )}
             </div>
           </div>
