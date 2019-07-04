cbf2eaed33f0c096b776f43625cf6a7394f4494f
emily <emilylu369@gmail.com>
2019-07-03 23:31:47 -0400

    fixed bug cant delete comment

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -138,7 +138,6 @@ export const addComment = (postId, formData) => async dispatch => {
     });
     dispatch(setAlert("Comment Added", "success"));
   } catch (err) {
-    console.log("heddr");
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
@@ -149,14 +148,15 @@ export const addComment = (postId, formData) => async dispatch => {
 // Delete comment
 export const deleteComment = (postId, commentId) => async dispatch => {
   try {
-    const res = await axios.post(`/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
     dispatch({
       type: REMOVE_COMMENT,
       payload: commentId
     });
-    dispatch(setAlert("Comment deleted", "success"));
+
+    dispatch(setAlert("Comment Removed", "success"));
   } catch (err) {
-    console.log("heddr");
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
